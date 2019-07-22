a1340e71f139e8b7bec816e7293181eef0aa510c
Ganapathy <g@localhost.localdomain>
2019-07-22 13:08:51 +0530

    Show lib name with version info at startup

--- cli.js
+++ cli.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
-const app = require('./dist')
+const app = require("./dist");
 
-console.log("@open-tech-world/create-react-app");
+const chalk = require("chalk");
+const { version } = require("./package.json");
 
-app.run()
+console.log(
+  chalk`\n\t{bold.rgb(255, 136, 0) @open-tech-world/create-react-app} {gray [v${version}]} \n`
+);
+
+app.run();
