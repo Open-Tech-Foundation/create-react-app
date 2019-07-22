62e5d3e02b2ff64cf194dabc018a9a188a7deab1
Ganapathy <g@localhost.localdomain>
2019-07-22 20:27:23 +0530

    Add questions with project name & typescript support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export function run() {
-  console.log("App is running");
+import path from "path";
+import inquirer from "inquirer";
+
+import questions from "./questions";
+
+export async function run() {
+  const pathToProject = process.argv.slice(2)[0];
+  const projectName: string = path.basename(pathToProject);
+  const options = await inquirer.prompt(questions(projectName));
+  console.log(options);
 }
---
+++ src/questions.ts
@@ -0,0 +1,18 @@
+import { Questions } from "inquirer";
+
+export default (projectName: string): Questions => {
+  return [
+    {
+      type: "input",
+      name: "projectName",
+      message: "Project name: ",
+      default: projectName
+    },
+    {
+      type: "confirm",
+      name: "typescript",
+      message: "Typescript support(.ts, .tsx)",
+      default: true
+    }
+  ];
+};
