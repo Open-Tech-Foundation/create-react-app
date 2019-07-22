64c7c79ece0211363f7587957b388b36e135a516
Ganapathy <g@localhost.localdomain>
2019-07-23 01:51:09 +0530

    Remove auth info from questions

--- src/run/questions.ts
+++ src/run/questions.ts
@@ -1,28 +1,18 @@
-import { Questions } from "inquirer";
+import { Questions } from 'inquirer';
 
 export default (projectName: string): Questions => {
   return [
     {
-      type: "input",
-      name: "projectName",
-      message: "Project name: ",
-      default: projectName
+      type: 'input',
+      name: 'projectName',
+      message: 'Project name: ',
+      default: projectName,
     },
     {
-      type: "confirm",
-      name: "typescript",
-      message: "Typescript support(.ts, .tsx)",
-      default: true
-    },
-    {
-      type: "input",
-      name: "authorFullName",
-      message: "Author full name: ",
-    },
-    {
-      type: "input",
-      name: "authorEmail",
-      message: "Author email: ",
+      type: 'confirm',
+      name: 'typescript',
+      message: 'Typescript support(.ts, .tsx)',
+      default: true,
     },
   ];
 };
