5007be1a85e38aab5feae898c08e001ec8eb5bb4
Ganapathy <g@localhost.localdomain>
2019-07-23 02:13:59 +0530

    Add webpack related files to templates

--- src/run/create.ts
+++ src/run/create.ts
@@ -7,11 +7,12 @@ import ejs from 'ejs';
 export default async function create(projectPath: string, options: object) {
   mkdirp.sync(projectPath);
   const templatesPath = path.join(__dirname, 'templates');
-  const paths = globby.sync('*', { cwd: templatesPath });
+  const paths = globby.sync('**', { cwd: templatesPath });
   paths.forEach(f => {
     const str = readFileSync(path.join(templatesPath, f), 'utf8');
     const compiledStr = ejs.render(str, options);
     const outFile = path.join(projectPath, f.replace('.ejs', ''));
+    mkdirp.sync(path.dirname(outFile));
     writeFileSync(outFile, compiledStr);
   });
 }
---
+++ src/templates/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <h1>@open-tech-world/create-react-app</h1>;
+};
