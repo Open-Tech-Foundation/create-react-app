13fa2b1f5c99eea210bbaea1fc4f5959228f52ee
Ganapathy <g@localhost.localdomain>
2019-07-23 12:43:06 +0530

    Add landing page drawing to templates

--- src/templates/src/App.js
+++ src/templates/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import LandingPageImg from '../media/undraw_landing_page.svg';
 
 export default () => {
-  return <h1>@open-tech-world/create-react-app</h1>;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', padding: '50px' }}>
+      <h1>@open-tech-world/create-react-app</h1>
+      <img src={LandingPageImg} alt="Landing page img" />
+    </div>
+  );
 };
